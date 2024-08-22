a15fbd6220b0c80c1f10af4675a4bf791d5ad422
Sakib Mia <sakib01181@gmail.com>
2024-08-22 12:45:16 +0600

    nav and footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/navbar";
+import Footer from "@/components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme='carDoctorTheme'>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../shared/navbar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    );
+};
+
+export default HomePage;
